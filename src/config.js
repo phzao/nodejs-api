a2864023e6088bf52198676e8b421ae069b45e7a
Paulo Henrique <mrcodecorp@gmail.com>
2019-10-21 08:30:57 -0300

    start

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = '<strong>{0}</strong>';
+
+module.exports = {
+    connectionString: 'mongodb://localhost:27017',
+    sendgridKey: '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',
+    userImagesBlobConnectionString: 'TBD'
+}
